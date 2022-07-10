79cba079ed78970cb4bd384004f7470f2a729995
ProgramingIsTheFuture <franciscosantos@MacBook-Pro.Home>
2022-07-10 13:46:18 +0100

    Adding cors headers and comments

--- backend/bin/main.ml
+++ backend/bin/main.ml
@@ -1,12 +1,16 @@
 open Cohttp_lwt_unix
 open Logs
 
+let debug = true
+
 let server = 
   set_level ~all:true (Some Info);
 
   (Server.make ~callback: 
-    (Middleware.logger 
-      Router.router) ()) 
+    (Middleware.cors
+      (if debug then Middleware.logger 
+        Router.router else Router.router))
+    ()) 
   |> Server.create ~mode:(`TCP (`Port 8000)) 
   ;;
 
--- backend/lib/middleware/middleware.ml
+++ backend/lib/middleware/middleware.ml
@@ -1,22 +1,44 @@
+open Cohttp.Response
 open Cohttp
 open Cohttp_lwt_unix
 open Lwt
 
-(* ( body |> Cohttp_lwt.Body.to_string >|= fun body -> *)
-(*   Printf.sprintf "Uri: %s\nMethod: %s\nHeaders\nHeaders: %s\nBody: %s" uri *)
-(*     meth headers body ) *)
-(* >>= fun body -> Server.respond_string ~status:`OK ~body ();; *)
-
+(* Logs all the endpoints *)
 let logger f _conn req body =
+  (* Handling the request *)
   let r: (Cohttp.Response.t * Cohttp_lwt.Body.t) Lwt.t = f _conn req body in
+  (* Getting the status code *)
   let status = match state r with
   | Return (v) ->
       let (ss, _) = v in
       Code.code_of_status ss.status
   | _ -> 0 in
+  (* Getting the path  *)
   let path = req |> Request.uri |> Uri.path in
+  (* Getting the method  *)
   let meth = req |> Request.meth |> Code.string_of_method in
+  (* Printing the information about this request *)
   let () = Lwt_fmt.printf "Status: %d Method: %s PATH: %s\n" status meth path |> ignore in
+  (* returning the response *)
   r;;
 
+(* Allow all origins to access this API *)
+let cors f _conn req body =
+  let r: (Cohttp.Response.t * Cohttp_lwt.Body.t) Lwt.t = f _conn req body in
 
+  match state r with
+  | Return (v) ->
+    let (resp, body) = v in
+
+    let headers = resp.headers in
+    let n_headers = Cohttp.Header.add headers "Access-Control-Allow-Origin" "*" in
+
+    Lwt.return (
+      {encoding = resp.encoding; 
+       headers = n_headers; 
+       version = resp.version; 
+       status = resp.status; 
+       flush = resp.flush 
+      }, 
+      body)
+  | _ -> r;;
